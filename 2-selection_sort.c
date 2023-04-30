33a0fe099f52cb1e6f84e64356feb117f06400dc
Javan <javan.o.otieno@gmail.com>
2023-04-30 04:59:28 +0300

    Sorting Algos

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap_arr - Swap two integers in an array.
+ * @num1: The first integer to swap.
+ * @num2: The second integer to swap.
+ */
+
+void swap_arr(int *num1, int *num2)
+{
+	int tmp;
+
+	tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
+}
+
+/**
  * selection_sort - Sort an array of integers in ascending order
  *                  using the selection sort algorithm.
  * @array: An array of integers.
