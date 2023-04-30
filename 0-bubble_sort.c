dbfe8039fff6175392fa0dfda13ed82455c1503e
Javan <javan.o.otieno@gmail.com>
2023-04-30 05:17:18 +0300

    Sorting Algos

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,6 +27,9 @@ void bubble_sort(int *array, size_t size)
 
 	size_t step, i;
 
+	if (array == NULL)
+		return;
+
 	for (step = 0; step < size - 1; ++step)
 	{
 		for (i = 0; i < size - step - 1; ++i)
